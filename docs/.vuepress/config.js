d60886ad42021b23e8e2b28dfae2b0f8e9403ec6
xitiantian <tiantian.xi@aminer.com>
2021-11-22 09:42:52 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,22 @@
 module.exports = {
   title: "一个小前端",
-  base:'/note/',
+  base: "/note/",
   // description: "Just playing around",
   markdown: {
     lineNumbers: true,
   },
   themeConfig: {
-    // nav: [
-    //   // 右上角的导航
-    //   { text: "Home", link: "/" },
-    //   { text: "Guide", link: "/guide/" },
-    //   { text: "External", link: "https://baidu.com" },
-    // ],
+    nav: [
+      // 右上角的导航
+      { text: "Home", link: "/" },
+      { text: "Best", link: "/best/" },
+      // { text: "External", link: "https://baidu.com" },
+    ],
     // sidebar: ["/", "/bar/", ["/foo/", "Explicit link text"]], // 侧边栏导航
-    sidebar: ['/', "/async"], // 侧边栏导航
+    // sidebar: ['/', "/async"], // 侧边栏导航
+    sidebar: {
+      "/best/": [""],
+      "/": ["", "async"],
+    },
   },
 };
