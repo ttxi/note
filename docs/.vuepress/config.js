b3af2b4711ccf32df27bd134597516047c604ab1
xitiantian <tiantian.xi@aminer.cn>
2022-05-07 23:34:54 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,22 +10,34 @@ module.exports = {
       { text: "Tx", link: "/best/" },
       { text: "Typescript", link: "/Typescript/" },
       { text: "Vue", link: "/vue/" },
+      { text: "Vue3", link: "/vue3/" },
+      { text: "React", link: "/react/" },
       {
         text: "other",
         items: [
           { text: "Linux", link: "/other/linux/" },
           { text: "MacOs", link: "/other/macos/" },
           { text: "Webpack", link: "/other/webpack/" },
+          { text: "组件开发", link: "/other/components/" }
         ]
       }
     ],
     sidebar: {
       "/vue/": [""],
+      "/vue3/": [
+        {
+          title: "vue3 整体架构",
+          collapsable: false,
+          children: [["/vue3/", "vue3架构介绍"]]
+        }
+      ],
+      "/react/": [""],
       "/Typescript/": ["", "htmlAndEvent"],
       "/best/": ["", "webApi", "css.initial", "lint"],
       "/other/linux/": ["", "vi", "auto", "shell", "linux-install"],
       "/other/macos/": [""],
       "/other/webpack/": [""],
+      "/other/components/": [""],
       "/": [
         "",
         "async",
